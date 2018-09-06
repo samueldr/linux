925cd612bafd0279e3c45983f76bc3d4d41cc215
Abhijit Kulkarni <kabhijit@codeaurora.org>
2018-09-06 10:47:14 -0700

    drm/msm/sde: update kickoff_cnt before ESD status check
    kickoff_cnt atomic variable is used to check if a commit is
    already kicked off and driver is waiting for tx_done. This
    variable should be reset in case of pingpong done timeout
    before checking the ESD failure to avoid recursive
    timeouts.
    
    Change-Id: I7f9062644cc3e926bba81442c98532de601ab6a0
    Signed-off-by: Abhijit Kulkarni <kabhijit@codeaurora.org>
    (cherry picked from commit d338fa3afaceb3e7bc607aaafbb62a83c7c640c3)

--- drivers/gpu/drm/msm/sde/sde_encoder_phys_cmd.c
+++ drivers/gpu/drm/msm/sde/sde_encoder_phys_cmd.c
@@ -473,11 +473,15 @@ static int _sde_encoder_phys_cmd_handle_ppdone_timeout(
 			to_sde_encoder_phys_cmd(phys_enc);
 	u32 frame_event = SDE_ENCODER_FRAME_EVENT_ERROR
 				| SDE_ENCODER_FRAME_EVENT_SIGNAL_RELEASE_FENCE;
+	struct drm_connector *conn;
+	int event;
+	u32 pending_kickoff_cnt;
 
 	if (!phys_enc || !phys_enc->hw_pp || !phys_enc->hw_ctl)
 		return -EINVAL;
 
 	cmd_enc->pp_timeout_report_cnt++;
+	pending_kickoff_cnt = atomic_read(&phys_enc->pending_kickoff_cnt);
 
 	if (sde_encoder_phys_cmd_is_master(phys_enc)) {
 		 /* trigger the retire fence if it was missed */
@@ -492,8 +496,11 @@ static int _sde_encoder_phys_cmd_handle_ppdone_timeout(
 
 	SDE_EVT32(DRMID(phys_enc->parent), phys_enc->hw_pp->idx - PINGPONG_0,
 			cmd_enc->pp_timeout_report_cnt,
-			atomic_read(&phys_enc->pending_kickoff_cnt),
+			pending_kickoff_cnt,
 			frame_event);
+
+	/* decrement the kickoff_cnt before checking for ESD status */
+	atomic_add_unless(&phys_enc->pending_kickoff_cnt, -1, 0);
 
 	/* check if panel is still sending TE signal or not */
 	if (sde_connector_esd_status(phys_enc->connector))
@@ -511,7 +518,7 @@ static int _sde_encoder_phys_cmd_handle_ppdone_timeout(
 				phys_enc->hw_pp->idx - PINGPONG_0,
 				phys_enc->hw_ctl->idx - CTL_0,
 				cmd_enc->pp_timeout_report_cnt,
-				atomic_read(&phys_enc->pending_kickoff_cnt));
+				pending_kickoff_cnt);
 
 		SDE_EVT32(DRMID(phys_enc->parent), SDE_EVTLOG_FATAL);
 	}
@@ -520,8 +527,6 @@ static int _sde_encoder_phys_cmd_handle_ppdone_timeout(
 	phys_enc->enable_state = SDE_ENC_ERR_NEEDS_HW_RESET;
 
 exit:
-	atomic_add_unless(&phys_enc->pending_kickoff_cnt, -1, 0);
-
 	if (phys_enc->parent_ops.handle_frame_done)
 		phys_enc->parent_ops.handle_frame_done(
 				phys_enc->parent, phys_enc, frame_event);
