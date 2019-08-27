a4cfd195dc2bbcad6d0fbab07cea8072b1eda69c
Peter Rosin <peda@axentia.se>
2019-08-27 11:09:26 +0000

    fbdev: fbmem: avoid exporting fb_center_logo
    The variable is only ever used from fbcon.c which is linked into the
    same module. Therefore, the export is not needed.
    
    Signed-off-by: Peter Rosin <peda@axentia.se>
    Reviewed-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Matthew Wilcox <willy@infradead.org>
    Signed-off-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190827110854.12574-4-peda@axentia.se
    (cherry picked from commit ab1c4c5e9d111a867964f4a67d9ab4a564d16b90)

--- drivers/video/fbdev/core/fbmem.c
+++ drivers/video/fbdev/core/fbmem.c
@@ -54,7 +54,6 @@ int num_registered_fb __read_mostly;
 EXPORT_SYMBOL(num_registered_fb);
 
 bool fb_center_logo __read_mostly;
-EXPORT_SYMBOL(fb_center_logo);
 
 static struct fb_info *get_fb_info(unsigned int idx)
 {
