d81bbc25be1bbd9d9b5c7569e8e104804f129fdd
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-10-05 10:20:30 -0400

    [FIH] FIH_I2C_LED hacks in cam_flash

--- drivers/media/platform/msm/camera/cam_sensor_module/cam_flash/cam_flash_core.c
+++ drivers/media/platform/msm/camera/cam_sensor_module/cam_flash/cam_flash_core.c
@@ -30,8 +30,13 @@ int cam_flash_prepare(struct cam_flash_ctrl *flash_ctrl,
 
 	if (regulator_enable &&
 		(flash_ctrl->is_regulator_enabled == false)) {
+#ifdef FIH_I2C_LED
+		rc = lm3644_flash_led_prepare(flash_ctrl->switch_trigger,
+			LM3644_ENABLE_REGULATOR, NULL);
+#else
 		rc = qpnp_flash_led_prepare(flash_ctrl->switch_trigger,
 			ENABLE_REGULATOR, NULL);
+#endif
 		if (rc) {
 			CAM_ERR(CAM_FLASH, "regulator enable failed rc = %d",
 				rc);
@@ -40,8 +45,13 @@ int cam_flash_prepare(struct cam_flash_ctrl *flash_ctrl,
 		flash_ctrl->is_regulator_enabled = true;
 	} else if ((!regulator_enable) &&
 		(flash_ctrl->is_regulator_enabled == true)) {
+#ifdef FIH_I2C_LED
+		rc = lm3644_flash_led_prepare(flash_ctrl->switch_trigger,
+			LM3644_DISABLE_REGULATOR, NULL);
+#else
 		rc = qpnp_flash_led_prepare(flash_ctrl->switch_trigger,
 			DISABLE_REGULATOR, NULL);
+#endif
 		if (rc) {
 			CAM_ERR(CAM_FLASH, "regulator disable failed rc = %d",
 				rc);
@@ -814,8 +824,13 @@ int cam_flash_parser(struct cam_flash_ctrl *fctrl, void *arg)
 			flash_query_info =
 				(struct cam_flash_query_curr *)cmd_buf;
 
+#ifdef FIH_I2C_LED
+			rc = lm3644_flash_led_prepare(fctrl->switch_trigger,
+				LM3644_QUERY_MAX_CURRENT, &query_curr_ma);
+#else
 			rc = qpnp_flash_led_prepare(fctrl->switch_trigger,
 				QUERY_MAX_CURRENT, &query_curr_ma);
+#endif
 			CAM_DBG(CAM_FLASH, "query_curr_ma = %d",
 				query_curr_ma);
 			if (rc) {
