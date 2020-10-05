3dff18df66ef8233ae3d17d7195325af642a6baf
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-10-05 11:28:26 -0400

    [oem] drm: sde_crtc: add tracing

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -3358,6 +3358,7 @@ static int _sde_crtc_flush_event_thread(struct drm_crtc *crtc)
 		SDE_DEBUG("no frames pending\n");
 		return 0;
 	}
+	SDE_ATRACE_BEGIN("crtc_wait_for_frame_done");
 
 	SDE_EVT32(DRMID(crtc), SDE_EVTLOG_FUNC_ENTRY);
 
@@ -3372,6 +3373,7 @@ static int _sde_crtc_flush_event_thread(struct drm_crtc *crtc)
 
 	SDE_EVT32_VERBOSE(DRMID(crtc), SDE_EVTLOG_FUNC_EXIT);
 
+	SDE_ATRACE_END("crtc_wait_for_frame_done");
 	return 0;
 }
 
