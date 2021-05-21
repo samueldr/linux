bab3d649f2c61509e22d7ffdd60d0e1d0eeea8e1
Vinod Koul <vkoul@kernel.org>
2021-05-21 15:24:40 +0530

    drm/msm/disp/dpu1: Add support for DSC
    Display Stream Compression (DSC) is one of the hw blocks in dpu, so add
    support by adding hw blocks for DSC
    
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/gpu/drm/msm/disp/dpu1/dpu_hw_catalog.h
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_hw_catalog.h
@@ -165,6 +165,7 @@ enum {
  * @DPU_PINGPONG_TE2        Additional tear check block for split pipes
  * @DPU_PINGPONG_SPLIT      PP block supports split fifo
  * @DPU_PINGPONG_SLAVE      PP block is a suitable slave for split fifo
+ * @DPU_PINGPONG_DSC        Display stream compression blocks
  * @DPU_PINGPONG_DITHER,    Dither blocks
  * @DPU_PINGPONG_MAX
  */
@@ -173,8 +174,19 @@ enum {
 	DPU_PINGPONG_TE2,
 	DPU_PINGPONG_SPLIT,
 	DPU_PINGPONG_SLAVE,
+	DPU_PINGPONG_DSC,
 	DPU_PINGPONG_DITHER,
 	DPU_PINGPONG_MAX
+};
+
+/**
+ * DSC sub-blocks
+ * @DPU_DSC		DSC sub block
+ * @DPU_DSC_MAX
+ */
+enum {
+	DPU_DSC = 0x1,
+	DPU_DSC_MAX
 };
 
 /**
@@ -413,6 +425,7 @@ struct dpu_dspp_sub_blks {
 struct dpu_pingpong_sub_blks {
 	struct dpu_pp_blk te;
 	struct dpu_pp_blk te2;
+	struct dpu_pp_blk dsc;
 	struct dpu_pp_blk dither;
 };
 
@@ -545,6 +558,16 @@ struct dpu_pingpong_cfg  {
 struct dpu_merge_3d_cfg  {
 	DPU_HW_BLK_INFO;
 	const struct dpu_merge_3d_sub_blks *sblk;
+};
+
+/**
+ * struct dpu_dsc_cfg - information of DSC blocks
+ * @id                 enum identifying this block
+ * @base               register offset of this block
+ * @features           bit mask identifying sub-blocks/features
+ */
+struct dpu_dsc_cfg {
+	DPU_HW_BLK_INFO;
 };
 
 /**
@@ -747,6 +770,9 @@ struct dpu_mdss_cfg {
 
 	u32 merge_3d_count;
 	const struct dpu_merge_3d_cfg *merge_3d;
+
+	u32 dsc_count;
+	struct dpu_dsc_cfg *dsc;
 
 	u32 intf_count;
 	const struct dpu_intf_cfg *intf;
