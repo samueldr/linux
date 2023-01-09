b0fe01911d704c4ba596b2c358a5a7e1f7e29301
Venkata Prasad Potturu <venkataprasad.potturu@amd.com>
2023-01-09 18:51:04 +0530

    ASoC: amd: acp: Enable i2s tdm support for skyrim platforms
    Enable i2s tdm support for skyrim platform using dmi quirks.
    
    Signed-off-by: Venkata Prasad Potturu <venkataprasad.potturu@amd.com>
    Link: https://lore.kernel.org/r/20230109132104.1259479-6-venkataprasad.potturu@amd.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/amd/acp/acp-mach-common.c
+++ sound/soc/amd/acp/acp-mach-common.c
@@ -36,6 +36,20 @@ static struct snd_soc_jack vg_headset;
 #define DUAL_CHANNEL	2
 #define FOUR_CHANNEL	4
 
+#define TDM_MODE_ENABLE 1
+
+const struct dmi_system_id acp_quirk_table[] = {
+	{
+		/* Google skyrim proto-0 */
+		.matches = {
+			DMI_EXACT_MATCH(DMI_PRODUCT_FAMILY, "Google_Skyrim"),
+		},
+		.driver_data = (void *)TDM_MODE_ENABLE,
+	},
+	{}
+};
+EXPORT_SYMBOL_GPL(acp_quirk_table);
+
 static struct snd_soc_jack pco_jack;
 
 static const unsigned int channels[] = {
