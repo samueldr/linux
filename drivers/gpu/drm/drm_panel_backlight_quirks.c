94d75d8cabf168b21a6940ad9404587b07810bc8
Samuel Dionne-Riel <samuel@dionne-riel.com>
2025-11-08 21:12:19 -0500

    drm: panel-backlight-quirks: Add Minisforum V3 SE brightness quirk
    This is a personal preference, I'm not even sure it will work.
    
    Why is this strictly implemented through DMI quirks? This is AWFUL!
    I would expect to be able to at least be given the option to try setting
    this to some values either through sysfs or through kernel/module
    parameters... Right now you need to rebuild the kernel or module just to
    test this one thing!
    
    (Or, why not a device tree overlay? Sir, even if this is a Wendy's, you
    totally could make this work as device tree overlay in an ACPI world.
    Having the data live as data, rather than “code” (data in structs) would
    be much better for users, and distro integrators alike.)
    
    Anyway... This can't even be upstreamable. Why? This should be
    classified as a personal preference. This is because ***I*** find that
    the minimum brightness is too bright. Who am I to decide that's fine for
    everyone else? I might even be overlooking something that the
    manufacturer is aware of, and being lucky with a good panel, while the
    forced lower brightness would cause issues for other systems!
    
    So now I have to fucking maintain an additional patch on top of the
    kernel. Great.
    
    Remind me: why am I staring in an abyss? Why am I writing this commit
    message? At what point in my life did it become an inevitability?

--- drivers/gpu/drm/drm_panel_backlight_quirks.c
+++ drivers/gpu/drm/drm_panel_backlight_quirks.c
@@ -45,6 +45,14 @@ static const struct drm_get_panel_backlight_quirk drm_panel_min_backlight_quirks
 		.ident.name = "NE135A1M-NY1",
 		.quirk = { .min_brightness = 1, },
 	},
+	/* Minisforum V3 SE */
+	{
+		.dmi_match.field = DMI_SYS_VENDOR,
+		.dmi_match.value = "Micro Computer (HK) Tech Limited",
+		.dmi_match_other.field = DMI_PRODUCT_NAME,
+		.dmi_match_other.value = "V3 SE",
+		.quirk = { .min_brightness = 1, },
+	},
 	/* Steam Deck models */
 	{
 		.dmi_match.field = DMI_SYS_VENDOR,
