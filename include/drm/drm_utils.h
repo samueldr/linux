49b52cbafb63c90683c5aa23d0f6394968c908ef
Samuel Dionne-Riel <samuel@dionne-riel.com>
2025-11-29 13:52:58 -0500

    drm: panel-backlight-quirks: Add quirk to disable custom brightness curve
    While unverified, through observation it seems that some AMD GPU
    devices' firmware provides default brightness curves that are not
    adapted to the specific device.
    
    When those curves are enabled, the resulting brightness control is much
    less granular than the hardware supports.
    
    On the specific device this was observed, this especially affected the
    lowest range of brightness value, where no nuance existed.
    
    This change results in a much smoother brightness curve at lower values,
    with no observable drawbacks. This was used while also setting the
    minimum backlight to (effectively) zero.
    
    Signed-off-by: Samuel Dionne-Riel <samuel@dionne-riel.com>

--- include/drm/drm_utils.h
+++ include/drm/drm_utils.h
@@ -19,6 +19,7 @@ int drm_get_panel_orientation_quirk(int width, int height);
 struct drm_panel_backlight_quirk {
 	u16 min_brightness;
 	u32 brightness_mask;
+	bool disable_custom_brightness_curve;
 };
 
 const struct drm_panel_backlight_quirk *
